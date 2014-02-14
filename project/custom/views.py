302c9ebcf67a26d1e4b6afb6a7364fcf4dcb9a11
Vince Carney <vincecarney@gmail.com>
2014-02-14 16:12:09 -0600

    Dev update

--- project/custom/views.py
+++ project/custom/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from custom.utils import round_down
+from instagallery.models import Image
+
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    instagram_images = Image.objects.order_by('-instagram_id')[:100]
+    rounded_down = round_down(instagram_images.count(), 10)
+    instagram_images = instagram_images[:rounded_down + 1]
+    return render(request, 'homepage.html', {'instagram_images': instagram_images})
