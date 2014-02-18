2e9b3d4668716f4281364ed0af86aed34a9cda34
Vince Carney <vincecarney@gmail.com>
2014-02-18 11:22:40 -0600

    Fix cache decorator

--- project/custom/views.py
+++ project/custom/views.py
@@ -5,9 +5,10 @@ from custom.utils import round_down
 from instagallery.models import Image
 
 
-cache_page(60 * 5)
+@cache_page(60 * 5)
 def homepage(request):
     instagram_images = Image.objects.order_by('-instagram_id')[:100]
     rounded_down = round_down(instagram_images.count(), 10)
     instagram_images = instagram_images[:rounded_down]
-    return render(request, 'homepage.html', {'instagram_images': instagram_images})
+    dict_context = {'instagram_images': instagram_images}
+    return render(request, 'homepage.html', dict_context)
