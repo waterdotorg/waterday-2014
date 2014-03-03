2e973b3d94ca305ddd5e2fe6539085d24a7837bb
Vince Carney <vincecarney@gmail.com>
2014-03-03 11:03:56 -0600

    Limit instagallery grid to 40 items max

--- project/custom/views.py
+++ project/custom/views.py
@@ -7,7 +7,7 @@ from instagallery.models import Image
 
 @cache_page(60 * 5)
 def homepage(request):
-    instagram_images = Image.objects.order_by('-instagram_id')[:60]
+    instagram_images = Image.objects.order_by('-instagram_id')[:40]
     rounded_down = round_down(instagram_images.count(), 10)
     instagram_images = instagram_images[:rounded_down]
     dict_context = {'instagram_images': instagram_images}
